cefbac23b2626b5375ad63f9e16d6293ee870bfb
woinshet1 <woinshet@DESKTOP-VDL1QKS.localdomain>
2022-03-30 06:11:50 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let newArr = [];
+    for (let i =0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(positiveNumber){
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+
+}
